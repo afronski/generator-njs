efb5321cf83e9100807f9a26b56357dcce38f0d4
Wojciech Gawronski <afronski@gmail.com>
2014-04-10 21:39:31 +0200

    Small reformatting.

--- app/index.js
+++ app/index.js
@@ -8,56 +8,56 @@ var util = require("util"),
     NjsGenerator;
 
 NjsGenerator = module.exports = function NjsGenerator(args, options) {
-  yeoman.generators.Base.apply(this, arguments);
+    yeoman.generators.Base.apply(this, arguments);
 
-  this.on("end", function () {
-    this.installDependencies({
-      skipInstall: options["skip-install"]
+    this.on("end", function () {
+        this.installDependencies({
+            skipInstall: options["skip-install"]
+        });
     });
-  });
 
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, "../package.json")));
+    this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, "../package.json")));
 };
 
 util.inherits(NjsGenerator, yeoman.generators.Base);
 
 NjsGenerator.prototype.askFor = function () {
-  var callback = this.async();
+    var callback = this.async();
 
-  console.log(this.yeoman);
+    console.log(this.yeoman);
 
-  var prompts = [{
-    name: "name",
-    message: "What do you want to call your application?",
+    var prompts = [{
+        name: "name",
+        message: "What do you want to call your application?",
 
-    default: "test-njs-application"
-  }];
+        default: "test-njs-application"
+    }];
 
-  this.prompt(prompts, function (props) {
-    this.name = props.name;
+    this.prompt(prompts, function (props) {
+        this.name = props.name;
 
-    callback();
-  }.bind(this));
+        callback();
+    }.bind(this));
 };
 
 NjsGenerator.prototype.app = function app() {
-  this.mkdir("src");
-  this.mkdir("test");
+    this.mkdir("src");
+    this.mkdir("test");
 
-  this.copy("Gruntfile.js", "Gruntfile.js");
-  this.copy("index.js", "index.js");
+    this.copy("Gruntfile.js", "Gruntfile.js");
+    this.copy("index.js", "index.js");
 
-  this.copy("src/fake.js", "src/fake.js");
-  this.copy("test/fake.test.js", "test/fake.test.js");
+    this.copy("src/fake.js", "src/fake.js");
+    this.copy("test/fake.test.js", "test/fake.test.js");
 
-  this.template("_package.json", "package.json");
+    this.template("_package.json", "package.json");
 };
 
 NjsGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy("License - MIT.md", "License - MIT.md");
+    this.copy("License - MIT.md", "License - MIT.md");
 
-  this.copy(".jshintrc", ".jshintrc");
-  this.copy(".gitignore", ".gitignore");
+    this.copy(".jshintrc", ".jshintrc");
+    this.copy(".gitignore", ".gitignore");
 
-  this.template("_README.md", "README.md");
+    this.template("_README.md", "README.md");
 };
